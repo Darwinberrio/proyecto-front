8636ca0aa5e8eb5ebd25933f1167efc46dfee00f
Darwin <tu.email@example.com>
2025-11-09 18:40:43 +0100

    ADD: HTML-CSS

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/* AL PROGRAMAR
+    - Orden
+    - organizacion
+    - separacion de responsabilidades */
+
+
+/* VARIABLES */
+
+
+/* capturar elementos DOM */
+
+
+/* EVENTOS */
+
+
+/* FUNCIONES */
+
+
+/* INVOCACIÓN A LAS FUNCIONES */
