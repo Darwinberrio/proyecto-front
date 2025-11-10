b74a950cb99eef3e0fc39c7b71b92023c66b4240
Melikadezfoli-Tech <melikadezfoli@gmail.com>
2025-11-10 09:24:25 +0100

    prueba branch

--- js/main.js
+++ js/main.js
@@ -5,6 +5,13 @@
 
 
 /* VARIABLES */
+const claveApi="KMDb7NM7edekXPlf2hLfhNSpsRtoXls5xEFzaWgjkMVUufY8ifnopuwJ";
+const baseUrl="https://www.pexels.com/es-es/api";
+const galeria=;
+const btnBusqueda=;
+const inputBusqueda=;
+const orientacion=;
+const favoritos=;
 
 
 /* capturar elementos DOM */
@@ -14,6 +21,10 @@
 
 
 /* FUNCIONES */
+cargarImagenes=()=>{
+
+};
+
 
 
 /* INVOCACIÓN A LAS FUNCIONES */
